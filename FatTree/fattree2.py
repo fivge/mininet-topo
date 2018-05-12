77cc7d87d1b5d7196735140894ba111031d74189
fivge <luanxingtong@gmail.com>
2018-05-12 17:20:17 +0800

    rm log.debug

--- FatTree/fattree2.py
+++ FatTree/fattree2.py
@@ -2,8 +2,8 @@ from mininet.topo import Topo
 import logging
 import os
 
-logging.basicConfig(filename='./fattree.log', level=logging.DEBUG)
-logger = logging.getLogger(__name__)
+# logging.basicConfig(filename='./fattree.log', level=logging.DEBUG)
+# logger = logging.getLogger(__name__)
 
 class FatTree( Topo ):
   
@@ -29,12 +29,12 @@ class FatTree( Topo ):
         Topo.__init__(self)
   
         self.createTopo()
-        logger.debug("Finished topology creation!")
+        # logger.debug("Finished topology creation!")
 
         self.createLink( bw_c2a=self.bw_c2a, 
                          bw_a2e=self.bw_a2e, 
                          bw_h2a=self.bw_h2a)
-        logger.debug("Finished adding links!")
+        # logger.debug("Finished adding links!")
 
     #    self.set_ovs_protocol_13()
     #    logger.debug("OF is set to version 1.3!")  
@@ -57,19 +57,15 @@ class FatTree( Topo ):
             switch_list.append(self.addSwitch('s' + PREFIX + str(x)))
 
     def createCoreLayerSwitch(self, NUMBER):
-        logger.debug("Create Core Layer")
         self._addSwitch(NUMBER, 1, self.CoreSwitchList)
 
     def createAggLayerSwitch(self, NUMBER):
-        logger.debug("Create Agg Layer")
         self._addSwitch(NUMBER, 2, self.AggSwitchList)
 
     def createEdgeLayerSwitch(self, NUMBER):
-        logger.debug("Create Edge Layer")
         self._addSwitch(NUMBER, 3, self.EdgeSwitchList)
 
     def createHost(self, NUMBER):
-        logger.debug("Create Host")
         for x in xrange(1, NUMBER+1):
             PREFIX = "h00"
             if x >= int(10):
@@ -82,7 +78,7 @@ class FatTree( Topo ):
     Add Link
     """
     def createLink(self, bw_c2a=0.2, bw_a2e=0.1, bw_h2a=0.5):
-        logger.debug("Add link Core to Agg.")
+        # logger.debug("Add link Core to Agg.")
         end = self.pod/2
         for x in xrange(0, self.iAggLayerSwitch, end):
             for i in xrange(0, end):
@@ -93,7 +89,7 @@ class FatTree( Topo ):
                         self.AggSwitchList[x+i],
                         **linkopts)
 
-        logger.debug("Add link Agg to Edge.")
+        # logger.debug("Add link Agg to Edge.")
         for x in xrange(0, self.iAggLayerSwitch, end):
             for i in xrange(0, end):
                 for j in xrange(0, end):
@@ -102,7 +98,7 @@ class FatTree( Topo ):
                         self.AggSwitchList[x+i], self.EdgeSwitchList[x+j],
                         **linkopts)
 
-        logger.debug("Add link Edge to Host.")
+        # logger.debug("Add link Edge to Host.")
         for x in xrange(0, self.iEdgeLayerSwitch):
             for i in xrange(0, self.density):
                 linkopts = dict(bw=bw_h2a) 
